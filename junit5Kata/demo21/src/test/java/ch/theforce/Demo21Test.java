6257dc0b8c32fbcd0390d491880ed85404e2cd65
daniel glenn <linuxbender@gmail.com>
2018-03-17 16:39:15 +0100

    chore: junit 5 setup

---
+++ junit5Kata/demo21/src/test/java/ch/theforce/Demo21Test.java
@@ -0,0 +1,12 @@
+package ch.theforce;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Demo21Test {
+
+    @Test
+    void CreateDemo21ProgramTest() {
+        assertEquals(2, 2);
+    }
+}
