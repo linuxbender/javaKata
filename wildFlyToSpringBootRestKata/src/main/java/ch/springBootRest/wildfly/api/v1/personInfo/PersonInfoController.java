8178bb2abec3f9898d5bdb61845e2695955661a0
daniel glenn <linuxbender@gmail.com>
2018-04-15 04:15:54 +0200

    refactor: change from T to ResponseEntity<?> Type

--- wildFlyToSpringBootRestKata/src/main/java/ch/springBootRest/wildfly/api/v1/personInfo/PersonInfoController.java
+++ wildFlyToSpringBootRestKata/src/main/java/ch/springBootRest/wildfly/api/v1/personInfo/PersonInfoController.java
@@ -1,14 +1,13 @@
 package ch.springBootRest.wildfly.api.v1.personInfo;
 
-import ch.springBootRest.wildfly.api.v1.personInfo.model.PersonStateEnum;
 import ch.springBootRest.wildfly.api.v1.personInfo.service.PersonInfoService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.Date;
 
 @RestController
@@ -23,32 +22,32 @@ public class PersonInfoController {
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/{personName}")
-    public String actionFullPersonName(@PathVariable String personName) {
-        return String.format("Hi, %s", personName);
+    public ResponseEntity<?> actionFullPersonName(@PathVariable String personName) {
+        return ResponseEntity.ok(String.format("Hi, %s", personName));
     }
 
     @RequestMapping(method = RequestMethod.GET)
-    public String actionFullPersonName() {
-        return this.personInfoService.getPersonName();
+    public ResponseEntity<?> actionFullPersonName() {
+        return ResponseEntity.ok(this.personInfoService.getPersonName());
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/person-states")
-    public ArrayList<PersonStateEnum> actionPersonStates() {
-        return this.personInfoService.getPersonStateCode();
+    public ResponseEntity<?> actionPersonStates() {
+        return ResponseEntity.ok(this.personInfoService.getPersonStateCode());
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/current-person-state")
-    public String actionCurrentPersonState() {
-        return this.personInfoService.isCurrentPersonActiv();
+    public ResponseEntity<?> actionCurrentPersonState() {
+        return ResponseEntity.ok(this.personInfoService.isCurrentPersonActiv());
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/is-state-count-valid")
-    public boolean isStateCountValid() {
-        return this.personInfoService.isStateCountValid();
+    public ResponseEntity<?> isStateCountValid() {
+        return ResponseEntity.ok(this.personInfoService.isStateCountValid());
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/current-date")
-    public Date actionCurrentDate() {
-        return new Date();
+    public ResponseEntity<?> actionCurrentDate() {
+        return ResponseEntity.ok(new Date());
     }
 }
--- wildFlyToSpringBootRestKata/src/main/java/ch/springBootRest/wildfly/api/v1/translation/TranslationController.java
+++ wildFlyToSpringBootRestKata/src/main/java/ch/springBootRest/wildfly/api/v1/translation/TranslationController.java
@@ -1,14 +1,12 @@
 package ch.springBootRest.wildfly.api.v1.translation;
 
-import ch.springBootRest.wildfly.api.v1.translation.dto.TranslationDto;
 import ch.springBootRest.wildfly.api.v1.translation.provider.TranslationProvider;
+import org.springframework.http.ResponseEntity;
 import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/translation")
@@ -22,17 +20,17 @@ public class TranslationController {
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/{translationKey}")
-    public TranslationDto actionTranslation(@PathVariable String translationKey) {
-        return this.translationProvider.getTranslation(translationKey);
+    public ResponseEntity<?> actionTranslation(@PathVariable String translationKey) {
+        return ResponseEntity.ok(this.translationProvider.getTranslation(translationKey));
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/{translationKey}/{replaceArgs}")
-    public TranslationDto actionTranslationWithReplaceArgs(@PathVariable String translationKey, @PathVariable String... replaceArgs) {
-        return this.translationProvider.getTranslation(translationKey, replaceArgs);
+    public ResponseEntity<?> actionTranslationWithReplaceArgs(@PathVariable String translationKey, @PathVariable String... replaceArgs) {
+        return ResponseEntity.ok(this.translationProvider.getTranslation(translationKey, replaceArgs));
     }
 
     @RequestMapping(method = RequestMethod.GET)
-    public List<TranslationDto> actionFullTranslation() {
-        return this.translationProvider.getFullTranslation();
+    public ResponseEntity<?> actionFullTranslation() {
+        return ResponseEntity.ok(this.translationProvider.getFullTranslation());
     }
 }
