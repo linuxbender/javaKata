56a0f83a2a59dd89c27a7c6a465bfacae3a165db
daniel glenn <linuxbender@gmail.com>
2018-04-16 04:32:19 +0200

    core: init spring config 101 Kata

---
+++ springConfig101Kata/src/main/java/ch/theforce/config101/Config101Application.java
@@ -0,0 +1,12 @@
+package ch.theforce.config101;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Config101Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Config101Application.class, args);
+	}
+}
