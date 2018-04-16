d6bfe3a99688c8b449ad672b6321243c755bb8fa
daniel glenn <linuxbender@gmail.com>
2018-04-16 06:11:53 +0200

    refactor: initialize bean with applicationContext registerBean

--- springConfig101Kata/src/main/java/ch/theforce/config101/Config101Application.java
+++ springConfig101Kata/src/main/java/ch/theforce/config101/Config101Application.java
@@ -1,11 +1,5 @@
 package ch.theforce.config101;
 
-import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
-import org.springframework.beans.factory.support.BeanDefinitionBuilder;
-import org.springframework.beans.factory.support.BeanDefinitionRegistry;
-import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContextInitializer;
@@ -13,27 +7,6 @@ import org.springframework.context.support.GenericApplicationContext;
 
 @SpringBootApplication
 public class Config101Application {
-
-    public static class MyBDRPP implements BeanDefinitionRegistryPostProcessor {
-
-        @Override
-        public void postProcessBeanDefinitionRegistry(BeanDefinitionRegistry registry) throws BeansException {
-
-            BeanFactory beanFactory = BeanFactory.class.cast(registry);
-
-            registry.registerBeanDefinition("baaService",
-                    BeanDefinitionBuilder.genericBeanDefinition(BaaService.class).getBeanDefinition());
-
-            registry.registerBeanDefinition("fooService",
-                    BeanDefinitionBuilder.genericBeanDefinition(FooService.class,
-                            () -> new FooService(beanFactory.getBean(BaaService.class))).getBeanDefinition());
-        }
-
-        @Override
-        public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
-
-        }
-    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(Config101Application.class, args);
@@ -44,8 +17,11 @@ class ProgrammaticBeanDefinitionInitializr implements ApplicationContextInitiali
 
     @Override
     public void initialize(GenericApplicationContext applicationContext) {
+
+        BaaService baaService = new BaaService();
+        FooService fooService = new FooService(baaService);
         applicationContext.registerBean(BaaService.class);
-        applicationContext.registerBean(FooService.class, () -> new FooService(applicationContext.getBean(BaaService.class)));
+        applicationContext.registerBean(FooService.class, () -> fooService);
     }
 }
 
