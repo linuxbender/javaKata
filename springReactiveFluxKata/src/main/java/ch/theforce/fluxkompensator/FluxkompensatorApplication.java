1ee8b48ed19d71f6bd009eee9c1f65c7252bc749
daniel glenn <linuxbender@gmail.com>
2018-04-16 02:30:57 +0200

    core: init SpringBoot Reactive Flux Kata

---
+++ springReactiveFluxKata/src/main/java/ch/theforce/fluxkompensator/FluxkompensatorApplication.java
@@ -0,0 +1,12 @@
+package ch.theforce.fluxkompensator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluxkompensatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FluxkompensatorApplication.class, args);
+	}
+}
---
+++ springReactiveFluxKata/src/test/java/ch/theforce/fluxkompensator/FluxkompensatorApplicationTests.java
@@ -0,0 +1,16 @@
+package ch.theforce.fluxkompensator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FluxkompensatorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
