e309f09df2c9f71aa6d2af46484efb177b645e6e
daniel glenn <linuxbender@gmail.com>
2018-04-16 04:57:37 +0200

    feat: manual register Bean in AppContext with tests

--- springConfig101Kata/src/main/java/ch/theforce/config101/Config101Application.java
+++ springConfig101Kata/src/main/java/ch/theforce/config101/Config101Application.java
@@ -1,12 +1,38 @@
 package ch.theforce.config101;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@Configuration
 public class Config101Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Config101Application.class, args);
+	@Bean
+	FooService fooService (BaaService baaService) {
+		return new FooService(baaService);
 	}
+
+	@Bean
+	BaaService baaService() {
+		return new BaaService();
+	}
+
+	public static void main(String[] args) {
+		//SpringApplication.run(Config101Application.class, args);
+		ApplicationContext ac = new AnnotationConfigApplicationContext(Config101Application.class);
+	}
+}
+
+class FooService {
+
+	private final BaaService baaService;
+
+	FooService(BaaService baaService) {
+		this.baaService = baaService;
+	}
+}
+
+class BaaService {
+
 }
--- springConfig101Kata/src/test/java/ch/theforce/config101/Config101ApplicationTests.java
+++ springConfig101Kata/src/test/java/ch/theforce/config101/Config101ApplicationTests.java
@@ -1,16 +1,24 @@
 package ch.theforce.config101;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@ContextConfiguration(classes = Config101Application.class)
 public class Config101ApplicationTests {
+
+    @Autowired
+    private ApplicationContext applicationContext;
 
 	@Test
 	public void contextLoads() {
+        Assert.assertNotNull("The FooSearvice should not be null",this.applicationContext.getBean(FooService.class));
+        Assert.assertNotNull("The BaaService should not be null",this.applicationContext.getBean(BaaService.class));
 	}
-
 }
