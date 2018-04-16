60ed835498957c76334fdf4506a0d144ff14aca8
daniel glenn <linuxbender@gmail.com>
2018-04-16 05:29:27 +0200

    refactor: using the low level case with BeanDefinitionRegistryPostProcessor

--- springConfig101Kata/src/main/java/ch/theforce/config101/Config101Application.java
+++ springConfig101Kata/src/main/java/ch/theforce/config101/Config101Application.java
@@ -1,5 +1,11 @@
 package ch.theforce.config101;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -7,14 +13,33 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
+import java.util.function.Supplier;
+
 @Configuration
 @ComponentScan
 public class Config101Application {
 
-	@Bean
-	BaaService baaService() {
-		return new BaaService();
-	}
+    @Component
+    public static class MyBDRPP implements BeanDefinitionRegistryPostProcessor {
+
+        @Override
+        public void postProcessBeanDefinitionRegistry(BeanDefinitionRegistry registry) throws BeansException {
+
+            BeanFactory beanFactory = BeanFactory.class.cast(registry);
+
+            registry.registerBeanDefinition("baaService",
+                    BeanDefinitionBuilder.genericBeanDefinition(BaaService.class).getBeanDefinition());
+
+            registry.registerBeanDefinition("fooService",
+                    BeanDefinitionBuilder.genericBeanDefinition(FooService.class,
+                            () -> new FooService(beanFactory.getBean(BaaService.class))).getBeanDefinition());
+        }
+
+        @Override
+        public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+
+        }
+    }
 
 	public static void main(String[] args) {
 		//SpringApplication.run(Config101Application.class, args);
@@ -22,7 +47,6 @@ public class Config101Application {
 	}
 }
 
-@Component
 class FooService {
 
 	private final BaaService baaService;
