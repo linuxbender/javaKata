9d9cecec27d4c1e8fcb9fdc42a3bbb1143d4a4ca
daniel glenn <linuxbender@gmail.com>
2018-04-20 23:36:42 +0200

    feat: springboot with redis and geo data

--- springRedisKata/src/main/java/ch/theforce/springRedis/SpringRedisApplication.java
+++ springRedisKata/src/main/java/ch/theforce/springRedis/SpringRedisApplication.java
@@ -1,37 +1,38 @@
 package ch.theforce.springRedis;
 
+import lombok.extern.java.Log;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.geo.*;
+import org.springframework.data.redis.connection.RedisGeoCommands;
+import org.springframework.data.redis.core.GeoOperations;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-import java.net.UnknownHostException;
-
+@Log
 @SpringBootApplication
 public class SpringRedisApplication {
 
-    public static class Cat {
+    private ApplicationRunner titleRunner(String title, ApplicationRunner rr) {
+        return args -> {
+            log.info(title.toUpperCase() + ":");
+            rr.run(args);
+        };
     }
 
     @Bean
-    @ConditionalOnMissingBean(name = "redisTemplate")
-    public RedisTemplate<String, Cat> redisTemplate(RedisConnectionFactory redisConnectionFactory) throws UnknownHostException {
-        RedisTemplate<String, Cat> template = new RedisTemplate<>();
+    ApplicationRunner geography(RedisTemplate<String, String> rt) {
+        return titleRunner("geography", args -> {
+            GeoOperations<String, String> geo = rt.opsForGeo();
+            geo.add("Sicily", new Point(13.361389, 38.1155556),"Arigento");
+            geo.add("Sicily", new Point(15.087269, 37.502669),"Catania");
+            geo.add("Sicily", new Point(13.583333, 37.3155556),"Palermo");
 
-        RedisSerializer<Cat> value = new Jackson2JsonRedisSerializer<>(Cat.class);
-        RedisSerializer keys = new StringRedisSerializer();
-
-        template.setConnectionFactory(redisConnectionFactory);
-        template.setKeySerializer(keys);
-        template.setValueSerializer(value);
-        template.setHashKeySerializer(keys);
-        template.setHashValueSerializer(value);
-        return template;
+            Circle circle = new Circle(new Point(13.583333, 37.316667),new Distance(100, RedisGeoCommands.DistanceUnit.KILOMETERS));
+            GeoResults<RedisGeoCommands.GeoLocation<String>> geoResult = geo.radius("Sicily", circle);
+            geoResult.getContent().forEach(c -> log.info(c.toString()));
+        });
     }
 
     public static void main(String[] args) {
