334c0c5bb158303dd8258bd54cf8433a200bad1b
daniel glenn <linuxbender@gmail.com>
2018-04-20 08:31:47 +0200

    feat: overwrite example for a template impl.

--- springRedisKata/src/main/java/ch/theforce/springRedis/SpringRedisApplication.java
+++ springRedisKata/src/main/java/ch/theforce/springRedis/SpringRedisApplication.java
@@ -2,11 +2,39 @@ package ch.theforce.springRedis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.net.UnknownHostException;
 
 @SpringBootApplication
 public class SpringRedisApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringRedisApplication.class, args);
-	}
+    public static class Cat {
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(name = "redisTemplate")
+    public RedisTemplate<String, Cat> redisTemplate(RedisConnectionFactory redisConnectionFactory) throws UnknownHostException {
+        RedisTemplate<String, Cat> template = new RedisTemplate<>();
+
+        RedisSerializer<Cat> value = new Jackson2JsonRedisSerializer<>(Cat.class);
+        RedisSerializer keys = new StringRedisSerializer();
+
+        template.setConnectionFactory(redisConnectionFactory);
+        template.setKeySerializer(keys);
+        template.setValueSerializer(value);
+        template.setHashKeySerializer(keys);
+        template.setHashValueSerializer(value);
+        return template;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRedisApplication.class, args);
+    }
 }
